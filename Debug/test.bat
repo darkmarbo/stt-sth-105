1731bd7a4c49bbad57f0c6b1a3376de736701374
darkmarbo <darkmarbo@163.com>
2016-10-28 16:39:04 +0800

    修改exe变量为两个 增加一个wav输出名字

--- Debug/test.bat
+++ Debug/test.bat
@@ -1,4 +1,9 @@
 tts_adap_105.exe labels_gen_rjl\\rs_rjl_100080.lab 
 tts_adap_105.exe labels_gen_rjl\\rs_rjl_100081.lab 
-tts_adap_105.exe labels_gen_rjl\\rs_rjl_100082.lab 
+tts_adap_105.exe labels_gen_rjl\\rs_rjl_100082.lab
+tts_adap_105.exe labels_gen_rjl\\rs_rjl_100083.lab 
+tts_adap_105.exe labels_gen_rjl\\rs_rjl_100084.lab 
+tts_adap_105.exe labels_gen_rjl\\rs_rjl_100085.lab 
+tts_adap_105.exe labels_gen_rjl\\rs_rjl_100086.lab  
+tts_adap_105.exe label.txt 
 pause
