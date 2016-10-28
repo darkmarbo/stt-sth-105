1e260dd05f22534904693f68414fadd52203f0ea
darkmarbo <darkmarbo@163.com>
2016-10-28 10:41:03 +0800

    删除src备份-包含中文字符的cl

--- tts_adap_105/src-路程/Char2Pinyin.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHAR2PINYIN_H_INCLUDED
-#define CHAR2PINYIN_H_INCLUDED
-
-
-void Char2Pinyin(char **wordseq,char **pinyinseq,int nword,int *npinyin,STable *wt,STable *ct);
-
-#endif // CHAR2PINYIN_H_INCLUDED
--- tts_adap_105/src-路程/ProsodicAnalysis.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROSODICANALYSIS_H_INCLUDED
-#define PROSODICANALYSIS_H_INCLUDED
-
-
-int GetWrodPosSegment(char *str,char **words,char **pos,int *nWords);
-void ProsodicWordAnalysis(char **wordseq,char **posseq,int nWords,int *result,Table *pwModel);
-void ProsodicPhraseAnalysis(char **wordseq,char **posseq,int nWords,int *pwresult,int *result,Table *ppModel,Table *ipModel);
-void GetProsodicTag(char **wordseq,int nWords,int *pwresult,int *ppresult,short *tag);
-#endif // PROSODICANALYSIS_H_INCLUDED
