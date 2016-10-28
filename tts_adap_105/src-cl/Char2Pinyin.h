ecd32a345dea234ef084dba4e407977574551c73
darkmarbo <darkmarbo@163.com>
2016-10-28 11:33:53 +0800

    添加src-lc备份

---
+++ tts_adap_105/src-cl/Char2Pinyin.h
@@ -0,0 +1,7 @@
+#ifndef CHAR2PINYIN_H_INCLUDED
+#define CHAR2PINYIN_H_INCLUDED
+
+
+void Char2Pinyin(char **wordseq,char **pinyinseq,int nword,int *npinyin,STable *wt,STable *ct);
+
+#endif // CHAR2PINYIN_H_INCLUDED
